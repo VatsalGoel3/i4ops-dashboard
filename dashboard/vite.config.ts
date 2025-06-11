90e2315ce4cb693e3b721a628f0fe3e3174470d3
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2025-06-11 14:06:48 -0600

    Updated port to 8888

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,        
-    port: 6000,           
+    port: 8888,           
     proxy: {
       '/api': {
         target: 'http://localhost:4000',

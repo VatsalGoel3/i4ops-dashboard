f2b0bd12bc1072dbc7b0a448bd5b4a880c54b893
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2025-06-16 11:14:41 -0600

    Updated to use UIProvider

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { UIProvider } from './context/UIContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+    <UIProvider>
       <App />
+    </UIProvider>
     </BrowserRouter>
   </React.StrictMode>
 );

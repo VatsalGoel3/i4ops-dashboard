b6d61dc6292008850730b2f01cdd6d592be2e0ea
Vatsal Goel <144617902+VatsalGoel3@users.noreply.github.com>
2025-06-20 09:13:54 -0600

    feat: add QueryClient provider and React Query DevTools
    - Wrap app with QueryClientProvider
    - Add dev tools for debugging queries
    - Enable global query state management

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App';
 import './index.css';
 import { UIProvider } from './context/UIContext';
+import { queryClient } from './lib/queryClient';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <UIProvider>
-      <App />
-    </UIProvider>
+      <QueryClientProvider client={queryClient}>
+        <UIProvider>
+          <App />
+        </UIProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
